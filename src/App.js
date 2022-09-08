8ba20a8f674deb40e875e530258312ac07a33e2b
whemmy <67226591+whemmy@users.noreply.github.com>
2022-09-08 12:43:58 +0100

    done

--- src/App.js
+++ src/App.js
@@ -31,7 +31,38 @@ function App() {
         <h2>experience</h2>
         <div className="underline"></div>
       </div>
-      <div className="jobs-center"></div>
+
+      <div className="jobs-center">
+        {/* btn container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h3>{company}</h3>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
     </section>
   )
 }
